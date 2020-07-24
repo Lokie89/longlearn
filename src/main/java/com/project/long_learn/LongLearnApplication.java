66401c17ebaf0be6f24cb269e67f289d64986ae7
Lokie89 <traeuman@gmail.com>
2020-07-24 14:28:22 +0900

    init

---
+++ src/main/java/com/project/long_learn/LongLearnApplication.java
@@ -0,0 +1,13 @@
+package com.project.long_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/long_learn/LongLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.long_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
