f856ae94911f6fc25030cbbb68a09571f103493e
Lokie89 <traeuman@gmail.com>
2020-09-10 16:30:12 +0900

    feat : Group, Study

--- src/main/java/com/project/long_learn/group/Study.java
+++ src/main/java/com/project/long_learn/group/Study.java
@@ -1,8 +1,8 @@
-package com.project.long_learn.club;
+package com.project.long_learn.group;
 
 import com.project.long_learn.Member;
 
-public class Study implements Club {
+public class Study implements Group {
     private int studyId;
 
     @Override
