ea639200fccf378e6ceeaac76aeec0c06f2a066f
Lokie89 <traeuman@gmail.com>
2020-09-11 16:15:11 +0900

    refactor : Study abstract class

--- src/main/java/com/project/long_learn/group/Study.java
+++ src/main/java/com/project/long_learn/group/Study.java
@@ -1,9 +1,18 @@
 package com.project.long_learn.group;
 
+import com.project.long_learn.apply.VolunteerSet;
 import com.project.long_learn.domain.Member;
 
-public class Study implements Group {
+import java.util.LinkedHashSet;
+
+public abstract class Study implements Group {
+
+    private VolunteerSet volunteerSet;
     private int studyId;
+
+    public Study(){
+        volunteerSet = new VolunteerSet(new LinkedHashSet<>());
+    }
 
     @Override
     public int involve(Member member) {
