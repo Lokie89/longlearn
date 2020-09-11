1cc8cfdd3c0c426ea25470deeb6f2a3fba6219d9
Lokie89 <traeuman@gmail.com>
2020-09-11 16:14:22 +0900

    feat : equals, hashCode override

--- src/main/java/com/project/long_learn/domain/Member.java
+++ src/main/java/com/project/long_learn/domain/Member.java
@@ -5,7 +5,29 @@ package com.project.long_learn.domain;
  * 데이터 베이스와 매핑 시킬 객체
  */
 public class Member {
+
+    // 임시
+    private int id;
+
+    public Member(int id) {
+        this.id = id;
+    }
+
     public void participate(int involveId) {
         // database save
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Member)) {
+            return false;
+        }
+        Member member = (Member) obj;
+        return id == member.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
