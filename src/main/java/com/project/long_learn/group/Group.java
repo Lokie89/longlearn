436e7db9882634c95f9012a5c8f76eb093c151b4
Lokie89 <traeuman@gmail.com>
2020-09-15 17:15:19 +0900

    feat : Group generic

--- src/main/java/com/project/long_learn/group/Group.java
+++ src/main/java/com/project/long_learn/group/Group.java
@@ -1,14 +1,12 @@
 package com.project.long_learn.group;
 
-import com.project.long_learn.apply.Apply;
-
 /**
  * 그룹.
  * 이 메서드는 포함할 객체를 매개 변수로 받아 객체의 상태를 변화 시켜줌
  * 변화된 객체의 일정 상태 값을 반환해줌
  */
-public interface Group<E extends Apply> {
-    int involve(E apply);
-    int except(E apply);
-    boolean isContain(E apply);
+public interface Group<E> {
+    int involve(E e);
+    int except(E e);
+    boolean isContain(E e);
 }
