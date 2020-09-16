245a20830e6721406d169e59218b0304eca9da26
Lokie89 <traeuman@gmail.com>
2020-09-16 15:44:33 +0900

    test : modify test condition

--- src/test/java/com/project/long_learn/apply/VolunteerTest.java
+++ src/test/java/com/project/long_learn/apply/VolunteerTest.java
@@ -1,11 +1,15 @@
 package com.project.long_learn.apply;
 
+import com.project.long_learn.condition.StudyInformation;
 import com.project.long_learn.domain.Member;
-import com.project.long_learn.group.LectureStudy;
 import com.project.long_learn.group.Study;
+import com.project.long_learn.group.StudyLocation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class VolunteerTest {
 
     Volunteer volunteer = new Volunteer(new Member(3), VolunteerRole.TEACHER);
-    Study study = new LectureStudy(6);
+    Study study = new Study(6, new StudyInformation.Builder(LocalDate.now(), LocalDate.now(), new HashSet<>(), new StudyLocation()).build());
 
     @BeforeEach
     void setUp() {
--- src/test/java/com/project/long_learn/group/StudyTest.java
+++ src/test/java/com/project/long_learn/group/StudyTest.java
@@ -1,15 +1,19 @@
 package com.project.long_learn.group;
 
-import com.project.long_learn.apply.VolunteerRole;
 import com.project.long_learn.apply.Volunteer;
+import com.project.long_learn.apply.VolunteerRole;
+import com.project.long_learn.condition.StudyInformation;
 import com.project.long_learn.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 class StudyTest {
 
-    Study study = new DebateStudy(5);
+    Study study = new Study(5, new StudyInformation.Builder(LocalDate.now(), LocalDate.now(), new HashSet<>(), new StudyLocation()).build());
 
     @BeforeEach
     void setUp() {
