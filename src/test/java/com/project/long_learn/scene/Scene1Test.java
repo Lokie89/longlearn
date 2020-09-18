a3311a405b270e8cb8be60fe1da9ef9b83e21a69
Lokie89 <traeuman@gmail.com>
2020-09-18 17:26:26 +0900

    test : lecture test

--- src/test/java/com/project/long_learn/scene/Scene1Test.java
+++ src/test/java/com/project/long_learn/scene/Scene1Test.java
@@ -20,13 +20,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Scene1Test {
     // SCENE #1 Test
-    StudyCondition.Builder defaultBuilder = new StudyCondition.Builder().start(LocalDate.now()).end(LocalDate.now()).studyDay(new StudyDays(new StudyDay(DayOfWeek.FRIDAY, LocalTime.NOON))).location(new StudyLocation(1,1,"강남"));
-    Study study = new Study(3, defaultBuilder.build());
-    Volunteer volunteer = new Volunteer(new Member(1), VolunteerRole.STUDENT);
+    StudyCondition.Builder defaultBuilder = new StudyCondition.Builder().start(LocalDate.now()).end(LocalDate.now()).studyDay(new StudyDays(new StudyDay(DayOfWeek.FRIDAY, LocalTime.NOON))).location(new StudyLocation(1, 1, "강남"));
+    Study study = new Study(3, defaultBuilder.maxStudent(3).description("꽃꽂이").minTeacher(1).maxTeacher(1).build());
 
     @BeforeEach
     void setUp() {
-
+        study.involve(new Volunteer(new Member(1), VolunteerRole.STUDENT));
+        study.involve(new Volunteer(new Member(2), VolunteerRole.STUDENT));
     }
 
     @AfterEach
@@ -35,7 +35,15 @@ public class Scene1Test {
     }
 
     @Test
+    void lectureStudy(){
+        study.involve(new Volunteer(new Member(3), VolunteerRole.STUDENT));
+        study.involve(new Volunteer(new Member(4), VolunteerRole.TEACHER));
+    }
+
+    @Test
     void 지원해야겠다() {
+        study.involve(new Volunteer(new Member(3), VolunteerRole.TEACHER));
+        Volunteer volunteer = new Volunteer(new Member(4), VolunteerRole.STUDENT);
         study.involve(volunteer);
         assertTrue(study.isContain(volunteer));
     }
