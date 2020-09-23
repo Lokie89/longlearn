fcc59def2c76e21ab0db470cf13da460d75fbddd
Lokie89 <traeuman@gmail.com>
2020-09-23 17:21:17 +0900

    feat : StudyTeacherArrangeException

---
+++ src/main/java/com/project/long_learn/condition/exception/StudyTeacherArrangeException.java
@@ -0,0 +1,7 @@
+package com.project.long_learn.condition.exception;
+
+import com.project.long_learn.group.StudyEssentialFieldNotSatisfiedException;
+
+public class StudyTeacherArrangeException extends StudyEssentialFieldNotSatisfiedException {
+
+}
