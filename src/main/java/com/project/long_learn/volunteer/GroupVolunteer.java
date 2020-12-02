814cb5b163066b710e16aee620a0bd1064dfb92d
Lokie89 <traeuman@gmail.com>
2020-12-02 17:14:12 +0900

    feat : GroupVolunteer 컬렉션 함수를 위한 hashCode equals 정의

--- src/main/java/com/project/long_learn/volunteer/GroupVolunteer.java
+++ src/main/java/com/project/long_learn/volunteer/GroupVolunteer.java
@@ -1,4 +1,9 @@
 package com.project.long_learn.volunteer;
 
 public abstract class GroupVolunteer implements Volunteer {
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public abstract boolean equals(Object obj);
 }
--- src/main/java/com/project/long_learn/volunteer/StudyGroupVolunteer.java
+++ src/main/java/com/project/long_learn/volunteer/StudyGroupVolunteer.java
@@ -1,4 +1,15 @@
 package com.project.long_learn.volunteer;
 
 public class StudyGroupVolunteer extends GroupVolunteer {
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
 }
