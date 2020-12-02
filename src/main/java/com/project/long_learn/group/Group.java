c15f5e5bdf9cae00ff984747b156f209e32c6803
Lokie89 <traeuman@gmail.com>
2020-12-02 16:00:44 +0900

    feat : volunteer, group interface

---
+++ src/main/java/com/project/long_learn/group/Group.java
@@ -0,0 +1,7 @@
+package com.project.long_learn.group;
+
+import com.project.long_learn.volunteer.Volunteer;
+
+public interface Group {
+    boolean contains(Volunteer volunteer);
+}
---
+++ src/main/java/com/project/long_learn/volunteer/Volunteer.java
@@ -0,0 +1,7 @@
+package com.project.long_learn.volunteer;
+
+import com.project.long_learn.group.Group;
+
+public interface Volunteer {
+    void apply(Group group);
+}
