6fb00d2cb2e44f4fb8cda98a2b8aaebdc3d79235
Lokie89 <traeuman@gmail.com>
2021-01-04 16:34:37 +0900

    feat : domain member Member class

--- src/main/java/com/project/long_learn/domain/member/Member.java
+++ src/main/java/com/project/long_learn/domain/member/Member.java
@@ -1,4 +1,33 @@
 package com.project.long_learn.domain.member;
 
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(of = {"name", "reporting"})
+@Getter
+@NoArgsConstructor
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ColumnDefault("0")
+    private Integer reporting;
+
+    @Builder
+    public Member(String name, int reporting) {
+        this.name = name;
+        this.reporting = reporting;
+    }
+
+    public void report() {
+        this.reporting++;
+    }
+
 }
