66ad790529208a92b360b75edfd4cafb0ed32c8b
Lokie89 <traeuman@gmail.com>
2021-01-04 14:59:18 +0900

    feat : IndexController, index mustache

--- src/main/java/com/project/long_learn/web/controller/IndexController.java
+++ src/main/java/com/project/long_learn/web/controller/IndexController.java
@@ -1,2 +1,13 @@
-package com.project.long_learn.web.controller;public class IndexController {
+package com.project.long_learn.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
+    }
 }
